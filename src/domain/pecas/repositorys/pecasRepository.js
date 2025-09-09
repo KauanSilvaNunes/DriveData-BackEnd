7ca11d6072f8e55d24e71d3fe2ed4094c4982462
Nickolas-Lobo <nickolas.lobo@outlook.com>
2025-09-09 11:10:42 -0300

    fetch(Conventendo a data para o formato utilizado no Brasil)

--- src/domain/pecas/repositorys/pecasRepository.js
+++ src/domain/pecas/repositorys/pecasRepository.js
@@ -1,23 +1,26 @@
-const db=require("../../../config/db");
+const db = require("../../../config/db");
 
+class Pecas {
+  
+  async todasPecas() {
+    const [row] = await db.query("SELECT * FROM pecas;");
+    return row;
+  }
 
-class Pecas{
-
-    async todasPecas(){
-        const [row]= await db.query("Select * from pecas;")
-        return row
-    }
-
-    async buscarManutencoes(){
-        const [row]= await db.query("SELECT i.ID, a.nome_automovel AS Nome_automovel, p.nome_peca AS Nome_peca, i.data_maxima, i.quilometragem_maxima, i.quilometragem_instalacao, i.data_instalacao FROM manutencao i LEFT JOIN pecas p ON i.ID_pecas = p.ID LEFT JOIN automovel a ON i.ID_automovel= a.ID;")
-        return row
-    }
-
-
-
+  async buscarManutencoes() {
+    const [row] = await db.query(`
+      SELECT 
+        i.ID, 
+        a.nome_automovel AS Nome_automovel, 
+        p.nome_peca AS Nome_peca, 
+        DATE_FORMAT(i.data_maxima, '%d/%m/%Y') AS data_maxima,
+        i.quilometragem_maxima, 
+        i.quilometragem_instalacao, 
+        DATE_FORMAT(i.data_instalacao, '%d/%m/%Y') AS data_instalacao
+      FROM manutencao i
+      LEFT JOIN pecas p ON i.ID_pecas = p.ID
+      LEFT JOIN automovel a ON i.ID_automovel = a.ID;
+    `);
+    return row;}
 }
-
-
-
-
-module.exports= new Pecas();
+module.exports = new Pecas();
